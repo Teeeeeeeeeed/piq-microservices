031bd3e98ceb589206dd8479bdb3eaf2f19f8fa5
tedtelo <ted.te.lo.ttl@gmail.com>
2023-08-28 22:46:51 +1200

    clean up ports

--- gateway/src/items/items.module.ts
+++ gateway/src/items/items.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ITEMS_PACKAGE_NAME, ITEMS_SERVICE_NAME } from './items.pb';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: ITEMS_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50052',
+          package: ITEMS_PACKAGE_NAME,
+          protoPath: 'node_modules/grpc-nest-proto/proto/items.proto',
+        },
+      },
+    ]),
+  ],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
--- gateway/src/user/user.module.ts
+++ gateway/src/user/user.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { USER_PACKAGE_NAME, USER_SERIVCE_SERVICE_NAME } from './user.pb';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: USER_SERIVCE_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50052',
+          package: USER_PACKAGE_NAME,
+          protoPath: 'node_modules/grpc-nest-proto/proto/user.proto',
+        },
+      },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
