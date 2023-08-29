87096cb9588b3634b5dab1a4ce65064de406bfa9
tedtelo <ted.te.lo.ttl@gmail.com>
2023-08-29 22:49:04 +1200

    auth service

--- auth/src/auth/auth.module.ts
+++ auth/src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { Auth } from './auth.entity';
+import { AuthService } from './service/auth.service';
+import { JwtService } from './service/jwt.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'dev',
+      signOptions: { expiresIn: '365d' },
+    }),
+    TypeOrmModule.forFeature([Auth]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtService, JwtStrategy],
 })
 export class AuthModule {}
